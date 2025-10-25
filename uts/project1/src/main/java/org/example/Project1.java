ac85eb93c95d8f0e3d360825ae4384f77d3a42f3
Agus1783 <17agus03@gmail.com>
2025-10-26 00:45:57 +0700

    buat ulang project1

---
+++ uts/project1/src/main/java/org/example/Project1.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Project1 {
+    static int hitung(int n) {
+        if (n <= 1) {
+            return 1;
+        } else { // debug teori, untuk menghitung faktorial penjumlahan
+            return n + hitung(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan angka : ");
+        try {
+            int angka = input.nextInt();
+
+            int hasil = hitung(angka);
+
+            if (hasil % 2 == 0) {
+                System.out.println("\nGenap-" + hasil);
+            }
+            else {
+                System.out.println("\nGanjil-" + (hasil + 1));
+            }
+        } catch (Exception e) {
+            System.out.println("Input tidak valid.");
+        }
+
+    }
+}
